5abc34cd22fc196de954144119bb9ec80ddf60fc
Matheus Coelho <matheuscoelho060@gmail.com>
2024-11-13 20:05:52 -0300

    Adding Edit Test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -38,7 +38,7 @@ describe("Radio Station App", () => {
       "contain.text",
       "Tocando -  A Fine Jazz Gumbo Radio"
     );
-    cy.wait(3000);
+    cy.wait(2000);
 
     cy.get("[data-testid=play-stop-button]").click();
     cy.get("[data-testid=currently-playing-text]").should(
@@ -71,5 +71,50 @@ describe("Radio Station App", () => {
     cy.get(
       "[data-testid=music-style-card-7fe99458-b6f2-4af0-95bc-e05977964622]"
     ).should("not.exist");
+  });
+
+  it("should allow the user to edit a station from favorites", () => {
+    cy.get("[data-testid=station-name-7fe99458-b6f2-4af0-95bc-e05977964622]")
+      .should("be.visible")
+      .click();
+    cy.get("[data-testid=station-name-9d69cc77-b698-40c0-8036-17cd1f09ca44]")
+      .should("be.visible")
+      .click();
+    cy.get("[data-testid=station-name-98152a6d-9a3d-46d7-8019-c8c03f29be38]")
+      .should("be.visible")
+      .click();
+
+    cy.get("[data-testid=toggle-drawer-button]").click();
+
+    cy.get(
+      "[data-testid=music-style-card-7fe99458-b6f2-4af0-95bc-e05977964622]"
+    )
+      .first()
+      .within(() => {
+        cy.get("[data-testid=edit-button]").click();
+      });
+
+    cy.get("[data-testid=edit-station-dialog]").should("exist");
+
+    cy.get("[data-testid=station-name-input]").type(" Test");
+    cy.get("[data-testid=station-country-input]").type(" Test");
+    cy.get("[data-testid=station-votes-input]").type("12");
+
+    cy.get("[data-testid=save-button]").should("exist").click();
+
+    cy.get(
+      "[data-testid=music-style-card-7fe99458-b6f2-4af0-95bc-e05977964622]"
+    )
+      .first()
+      .within(() => {
+        cy.get("[data-testid=station-name]").should(
+          "contain.text",
+          "BBC Radio 5 Live Proper Test"
+        );
+        cy.get("[data-testid=station-info]").should(
+          "contain.text",
+          "The United Kingdom Of Great Britain And Northern Ireland Test,"
+        );
+      });
   });
 });
