88d87e97b47c75707851407ad7ea4b88c415e998
Matheus Coelho <matheuscoelho060@gmail.com>
2024-11-13 20:10:00 -0300

    Filter by name,country or language

--- src/services/get-radios.ts
+++ src/services/get-radios.ts
@@ -4,11 +4,18 @@ import { RadioStation } from "./types";
 interface RadioStations {
   stations: RadioStation[];
 }
-//Coloquei filtro por name apenas, pois se colocar country e lang ele faz combinar ao inves de fazer por ou. Com nome ele ja filtra e busca legal
 
-export const fetchStations = async (searchQuery: string): Promise<RadioStations> => {
+export const fetchStations = async (
+  searchQuery: string
+): Promise<RadioStations> => {
   try {
-    const response = await api.get(`/stations/search?limit=${10}&name=${searchQuery}`);
+    const params = {
+      name: searchQuery,
+      country: searchQuery,
+      language: searchQuery,
+    };
+
+    const response = await api.get(`/stations/search?limit=${10}`, { params });
 
     const stations = response.data;
 
